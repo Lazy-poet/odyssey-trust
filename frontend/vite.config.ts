dc547f1101c66eb68f0bc37f395ca54d5c26260c
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-07-19 13:43:37 +0100

    fix build error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "../backend/web_build/",
-    assetsDir: "static"
-  },
-  plugins: [react(), tsconfigPaths()]
-})
+  plugins: [react(), tsconfigPaths()],
+});
